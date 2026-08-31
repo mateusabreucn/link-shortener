ec6fa0ff154145b57c96562e1be3f46ab81b31de
MateusAbreu <mateusabreucn@gmail.com>
2026-08-31 09:20:53 -0300

    init: backend setup with database and orm setup

---
+++ apps/api/drizzle/schema.ts
@@ -0,0 +1,17 @@
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const links = pgTable("links", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  originalUrl: text("original_url").notNull(),
+  code: varchar("code", { length: 10 }).unique().notNull(),
+  clickCount: integer("click_count").default(0).notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+});
