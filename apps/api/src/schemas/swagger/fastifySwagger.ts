a571365acb294f6e3c0d0d2def39cccb47456265
MateusAbreu <mateusabreucn@gmail.com>
2026-09-05 18:29:46 -0300

    chore: versioning

--- apps/api/src/schemas/swagger/fastifySwagger.ts
+++ apps/api/src/schemas/swagger/fastifySwagger.ts
@@ -4,7 +4,7 @@ const fastifySwaggerSchema = {
     info: {
       title: "Link Shortener",
       description: "Link Shortener from your mate",
-      version: "0.0.1",
+      version: "0.1.0",
     },
   },
 };
